f5f6c8a53bf67d1eb76095a08f592a7da4c0878d
Ilkin Ibrahimov <99792912+ilkinibra@users.noreply.github.com>
2023-10-03 23:47:37 +0400

    Add files via upload

---
+++ Task 1.3/note.js
@@ -0,0 +1,15 @@
+let num1=10;
+let num2=60;
+let num3=30;
+
+if(num1>num2 && num1>num3)
+{
+    console.log(num1)
+}
+else if(num2>num1 && num2>num3)
+{
+    console.log(num2)
+}
+else{
+    console.log(num3)
+}
